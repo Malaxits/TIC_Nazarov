c438e74b9f372effe12611c11226c9d926d6a595
Malaxits <o.i.nazarov@student.khai.edu>
2024-05-02 14:45:00 +0300

    signal completed

--- SignalProcessing/pract2.py
+++ SignalProcessing/pract2.py
@@ -1,33 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-# Nazarov O.I 529
+
 # Вхідні параметри
-n = 500  # Довжина сигналу у відліках
-Fs = 1000  # Частота дискретизації (Гц)
-F_max = 15  # Максимальна частота сигналу (Гц)
+n = 500
+Fs = 1000
+F_max = 15
 
 # Генерація випадкового сигналу
 random_signal = np.random.normal(0, 10, n)
 
 # Визначення відліків часу
-time = np.arange(n) / Fs
-
-# Розрахунок параметрів фільтру
-cutoff_freq = F_max / (Fs / 2)  # Визначення частоти відсіву
-filter_order = 3  # Порядок фільтру
-sos = signal.butter(filter_order, cutoff_freq, 'low', output='sos')  # Розрахунок коефіцієнтів фільтру
+time = np.linspace(0, n / Fs, n)
 
 # Фільтрація сигналу
-filtered_signal = signal.sosfiltfilt(sos, random_signal)
+b, a = signal.butter(3, F_max / (Fs / 2), 'low')
+filtered_signal = signal.filtfilt(b, a, random_signal)
 
 # Побудова графіку сигналу
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.plot(time, filtered_signal, linewidth=1)
-ax.set_xlabel('Час, с', fontsize=14)
-ax.set_ylabel('Амплітуда', fontsize=14)
+plt.figure(figsize=(10, 6))
+plt.plot(time, filtered_signal, linewidth=1)
+plt.xlabel('Час, с', fontsize=14)
+plt.ylabel('Амплітуда', fontsize=14)
 plt.title('Фільтрований сигнал', fontsize=14)
-plt.savefig('./figures/filtered_signal.png', dpi=600)  # Збереження графіку
+plt.savefig('./figures/filtered_signal.png', dpi=600)
 plt.show()
 
 # Розрахунок спектру сигналу
@@ -37,12 +33,11 @@ freq = np.fft.fftfreq(n, 1/Fs)
 freq = np.fft.fftshift(freq)
 
 # Побудова графіку спектру
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.plot(freq, spectrum, linewidth=1)
-ax.set_xlabel('Частота, Гц', fontsize=14)
-ax.set_ylabel('Амплітуда', fontsize=14)
+plt.figure(figsize=(10, 6))
+plt.plot(freq, spectrum, linewidth=1)
+plt.xlabel('Частота, Гц', fontsize=14)
+plt.ylabel('Амплітуда', fontsize=14)
 plt.title('Спектр сигналу', fontsize=14)
-plt.savefig('./figures/signal_spectrum.png', dpi=600)  # Збереження графіку
+plt.savefig('./figures/signal_spectrum.png', dpi=600)
 plt.show()
-
-#...
+#кінець коду
